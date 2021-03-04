938c68718340d337ce025cbe0a18092af9d83396
Bryan Tong <tongb@vmware.com>
2021-03-03 18:41:38 -0800

    Barebones CLI

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+#[derive(Debug)]
+struct CustomError(String);
+
+
+fn main() -> Result<()> {
+    let path = "test.txt";
+    let content = std::fs::read_to_string(path)
+        .with_context(|| format!("could not read file `{}`", path))?;
+    println!("file content: {}", content);
+    Ok(())
 }
